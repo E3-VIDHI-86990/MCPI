9c6508331a37928e0ef94943f9867590bb4765db
vidhishende24 <vidhishende222@gmail.com>
2024-09-21 18:21:22 +0530

    Assign3_2

---
+++ Assign3_2/Debug/Src/Assign3_2.d
@@ -0,0 +1,13 @@
+Src/Assign3_2.o: ../Src/Assign3_2.c ../Inc/Assign3_2.h ../Inc/stm32f4xx.h \
+ ../Inc/stm32f407xx.h ../Inc/core_cm4.h ../Inc/cmsis_version.h \
+ ../Inc/cmsis_compiler.h ../Inc/cmsis_gcc.h ../Inc/mpu_armv7.h \
+ ../Inc/system_stm32f4xx.h
+../Inc/Assign3_2.h:
+../Inc/stm32f4xx.h:
+../Inc/stm32f407xx.h:
+../Inc/core_cm4.h:
+../Inc/cmsis_version.h:
+../Inc/cmsis_compiler.h:
+../Inc/cmsis_gcc.h:
+../Inc/mpu_armv7.h:
+../Inc/system_stm32f4xx.h:
---
+++ Assign3_2/Inc/Assign3_2.h
@@ -0,0 +1,27 @@
+/*
+ * Assign3_2.h
+ *
+ *  Created on: Sep 21, 2024
+ *      Author: vidhi
+ */
+
+#ifndef ASSIGN3_2_H_
+#define ASSIGN3_2_H_
+
+#include "stm32f4xx.h"
+
+#define GPIO_LED		GPIOD
+#define LED_GREEN_PIN	12
+#define LED_ORANGE_PIN	13
+#define LED_RED_PIN		14
+#define LED_BLUE_PIN	15
+
+#define GPIO_LED_CLKEN	3
+
+void LedInit(uint32_t pin);
+void LedOn(uint32_t pin);
+void LedOff(uint32_t pin);
+void LedBlink(uint32_t pin, uint32_t delayMs);
+
+
+#endif /* ASSIGN3_2_H_ */
